99f32fc941fd617b413f80e47c135ed2b9f04c65
Manuel19842022 <112113280+Manuel19842022@users.noreply.github.com>
2023-09-24 21:01:20 -0600

    Modificacion de tema del body y ul

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,53 @@
 'use client'
-import Image from 'next/image'
+import Link from "next/link"
 import React, { useState, useEffect } from 'react';
+
+const misTareas=[];
 
 export default function Home() {
 
-  const listaTareas=[];
+  const [Tarea, setTarea] = useState();
+  const [count,setCount]=useState(0)
+  const miInput=document.getElementById("InputTarea");
 
-  const agregaTarea=()=>{
-    let selInput=document.getElementById("first_name");
-    if(selInput!==null){
-      listaTareas.push({"id":listaTareas.length,"Tarea":selInput.value});
-      console.log(listaTareas)
-      const Lista=document.getElementById("ListaTareas");
-      Lista.className="list-none md:list-disc";
-      const claves=Object.keys(listaTareas);
-      const li=document.createElement('li');
+    useEffect(() => {
+        const miInput=document.getElementById("InputTarea");
+        const Lista=document.getElementById("ListaTareas");
+       // console.log(misTareas)
+        if(count!==0){
+           
+            const li=document.createElement('li');
+            li.className="miLi flex space-x-3 items-center";
+            let textNode=document.createTextNode(miInput.value);
+            li.appendChild(textNode)
+            Lista.appendChild(li)
+            miInput.value=""
+        }
+    });
 
-      const p=document.createElement('p');
-      p.appendChild(document.createTextNode(selInput.value));
-      Lista.appendChild(li).appendChild(p);
-
-    }
-    else{
-      console.log("No hay tarea que agregar..")
-    }
-  }
- 
   return (
+    <div className="centrar">
+      <div className="">
+        <h1 className="font-bold uppercase ...">Listas de Tareas</h1>
+        <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="relative">
+            <input type="text" id="InputTarea" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nueva Tarea" required/>
+            <button className="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"  onClick={() =>{
+              setCount(count + 1) 
+              setTarea({"Tarea":count});
+              miInput ?   misTareas.push([{"Tarea":count,"Descripcion":miInput.value}]) : console.log("Error");
+            }}>
+            Agregar Tarea
+            </button>
+          </div>
 
-      <div className="grid gap-6 mb-6 md:grid-cols-2">
-        <div className='flex flex-row'>
-          <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Escriba la tarea" required/>
-          <button type="button" onClick={agregaTarea} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agregar Tarea</button>
-        </div>
-        <div className='flex flex-row'>
-          <ul id="ListaTareas">
-
-          </ul>
+          <div className="relative">
+            <ul role="list" className="my-7" id="ListaTareas">
+            </ul>
+          </div>
         </div>
       </div>
- 
-  )
+    </div>
+
+  );
 }
