2d160c040e518d958a61253d1acb7ced19fd798f
Manuel19842022 <112113280+Manuel19842022@users.noreply.github.com>
2023-09-26 23:13:39 -0600

    Tareas Finalizadas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,33 @@ import React, { useState, useEffect } from 'react';
 const misTareas=[];
 
 export default function Home() {
+  const [nombreTarea,setNombreTarea]=useState("");
+  const [listaTarea, setListaTarea] = useState([]);
+  const [Llaves,setLlaves]=useState(0);
+/********************************  */
+ 
+const nuevaLlave=()=>{
+  console.log("click");
+  setLlaves(Llaves+1);
+  listaTarea.push({"Id":Llaves,"Nombre":nombreTarea});
+  setNombreTarea("");
+  //setLlaves(Llaves+1);
+}
 
-  const [Tarea, setTarea] = useState();
+useEffect(() => {
+  console.log(listaTarea)
+
+})
+
+
+
+
+/********************************* */
+ /* const [Tarea, setTarea] = useState();
   const [count,setCount]=useState(0)
   const miInput=document.getElementById("InputTarea");
-
-    useEffect(() => {
+*/
+   /* useEffect(() => {
         const miInput=document.getElementById("InputTarea");
         const Lista=document.getElementById("ListaTareas");
        // console.log(misTareas)
@@ -28,42 +49,59 @@ export default function Home() {
             divContenedorNombreTarea.appendChild(textNode);
             divContenedorNombreTarea.className="espacios";
             divTareas.appendChild(divContenedorVerificacion);
-            divTareas?.appendChild(divContenedorNombreTarea);
+            divTareas.appendChild(divContenedorNombreTarea);
             miInput.value="";
-           /* const li=document.createElement('li');
-            const li1=document.createElement('li');
-            li.className="miLi flex space-x-3 items-center";
-            let textNode=document.createTextNode(miInput.value);
-            li.appendChild(textNode)
-            const casillaVerificacion=document.createElement('input');
-            casillaVerificacion.type="checkbox";
-            casillaVerificacion.className="appearance-none checked:bg-blue-500 ...";
-            li1.appendChild(casillaVerificacion);
-            Lista.appendChild(li);
-            Lista.appendChild(li1);
-            miInput.value=""*/
+         
         }
-    });
+    });*/
 
+
+//e => setNombreTarea(e.target.value)
   return (
     <div className="centrar">
-      <div className="">
         <h1 className="font-bold uppercase ...">Listas de Tareas</h1>
         <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
           <div className="relative">
-            <input type="text" id="InputTarea" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nueva Tarea" required/>
-            <button className="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button"  onClick={() =>{
-              setCount(count + 1) 
-              setTarea({"Tarea":count});
-              miInput ?   misTareas.push([{"Tarea":count,"Descripcion":miInput.value}]) : console.log("Error");
-            }}>
-            Agregar Tarea
+            <input type="text" value={nombreTarea}  onChange={e => setNombreTarea(e.target.value)} id="InputTarea" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nueva Tarea" required/>
+            <button type="button" onClick={nuevaLlave} className="absolute top-0 right-0 p-2.5 h-full text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" disabled={nombreTarea ? "" : "disabled"}>
+              Agregar Tarea
             </button>
           </div>                                
-          <div id="contenedorTareas" className="grid grid-cols-2 gap-1">
-          </div>
+          
+            {
+              
+              listaTarea.map(Tarea=>(
+                  <div id="contenedorTareas" className="grid grid-cols-4 gap-1" key={Tarea.Id}>
+                    <div className="espacios">
+                      <input id="default-checkbox" type="checkbox"/>
+                    </div>
+                    <div className="centrarDiv">
+                    {
+                      Tarea.Nombre
+                    }
+                    </div>
+                    <div className="espacios">
+                      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+                        Editar
+                      </button>
+                    </div>
+                    <div className="espaciosDos">
+                        <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded ">
+                          Eliminar
+                        </button>
+                      </div>
+                  </div>
+                 )
+               )
+            }
+          
         </div>
-      </div>
+      
+      <div>
+
+      
+    </div>
+
     </div>
 
   );
